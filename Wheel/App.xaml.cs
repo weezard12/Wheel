02293b530cb7c3ba997699556e25975e0d1f1b6a
weezard12 <weezard1234@gmail.com>
2025-01-04 10:32:45 +0200

    Add project files.

---
+++ Wheel/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Wheel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Wheel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wheel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
