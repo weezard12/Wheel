ff083471b6bb6b3cfb594ab6d424a1a98313c866
weezard1234@gmail.com <weezard1234@gmail.com>
2025-01-09 08:42:56 +0200

    added maui community toolkit

--- Wheel/MauiProgram.cs
+++ Wheel/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Wheel
 {
@@ -8,7 +9,8 @@ namespace Wheel
         {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>()
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit() // Initialize the .NET MAUI Community Toolkit
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
