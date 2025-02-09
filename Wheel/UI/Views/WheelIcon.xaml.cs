b0b6db598a639a0a3d8fb739574d70ee38f4026e
weezard12 <weezard1234@gmail.com>
2025-02-09 20:27:19 +0200

    added donation button

--- Wheel/UI/Views/WheelIcon.xaml.cs
+++ Wheel/UI/Views/WheelIcon.xaml.cs
@@ -5,5 +5,30 @@ public partial class WheelIcon : ContentView
 	public WheelIcon()
 	{
 		InitializeComponent();
+        CogImage.Loaded += (a, b) => StartCogAnimation();
+        BackCogImage.Loaded += (a, b) => StartBackCogAnimation();
 	}
+    private async void StartCogAnimation()
+    {
+        while (CogImage.IsVisible) // Infinite loop for continuous rotation
+        {
+            await CogImage.RotateTo(360, 6000, Easing.SinInOut); // 1 second per full rotation
+            CogImage.Rotation = 0; // Reset to prevent overflow issues
+        }
+    }
+    private async void StartBackCogAnimation()
+    {
+        while (CogImage.IsVisible) // Infinite loop for continuous rotation
+        {
+            await BackCogImage.RotateTo(-360, 8000, Easing.SinInOut); // 1 second per full rotation
+            BackCogImage.Rotation = 0; // Reset to prevent overflow issues
+        }
+    }
+    private async void OnImageTapped(object sender, EventArgs e)
+    {
+        string url = "https://ko-fi.com/weezard12";
+        await Launcher.OpenAsync(url);
+    }
+
+
 }
